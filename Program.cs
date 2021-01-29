44589a700ab6d87ebc3be0fa50dd5a57fd1db1ff
NDTSTN <indietasten@gmail.com>
2021-01-29 22:01:50 +0100

    Add peak value visualization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NAudio.Wave;
 
 namespace CliSpectogram.NET
@@ -6,6 +7,7 @@ namespace CliSpectogram.NET
     class Program
     {
         static int? selectedInputDeviceIndex = null;
+        static WaveInEvent inputDevice;
         static int Main(string[] args)
         {
             Console.WriteLine("Welcome to the CLI Spectogram.NET Tool");
@@ -15,10 +17,67 @@ namespace CliSpectogram.NET
             {
                 return returnCode;
             }
+            Console.WriteLine($"Selected Device: {selectedInputDeviceIndex.Value}");
 
-            Console.Write($"Selected Device: {selectedInputDeviceIndex.Value}");
+            using (inputDevice = new WaveInEvent())
+            {
+                inputDevice.DeviceNumber = selectedInputDeviceIndex.Value;
+                inputDevice.BufferMilliseconds = 50;
+                PrintInputDeviceSettings();
+
+                Console.WriteLine("To start the visualization, press a key...");
+                Console.ReadKey(true);
+
+                inputDevice.DataAvailable += InputDevice_DataAvailable;
+                inputDevice.StartRecording();
+
+                Console.WriteLine("Listening on your input device. Press a key to stop...");
+                Console.ReadKey(true);
+                
+                inputDevice.StopRecording();
+                inputDevice.DataAvailable -= InputDevice_DataAvailable;
+
+                Console.WriteLine("Done recording.");
+            }
 
             return 0;
+        }
+
+        private static void PrintInputDeviceSettings()
+        {
+            Console.WriteLine("Listener settings:");
+            Console.WriteLine($" WaveFormat: {inputDevice.WaveFormat}");
+            Console.WriteLine($" BufferCount: {inputDevice.NumberOfBuffers}");
+            Console.WriteLine($" BufferMilliseconds: {inputDevice.BufferMilliseconds}");
+            Console.WriteLine("");
+        }
+
+        private static void InputDevice_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // peak renderer
+            float max = 0;
+            var buffer = new WaveBuffer(e.Buffer);
+            // interpret as 16 bit signed point audio
+            for (int index = 0; index < e.BytesRecorded / 2; index++)
+            {
+                var sample = buffer.ShortBuffer[index];
+
+                float sample32 = sample / (float)short.MaxValue;
+                // absolute value 
+                if (sample32 < 0)
+                    sample32 = -sample32;
+                // is this the max value?
+                if (sample32 > max)
+                    max = sample32;
+            }
+
+            int maxWidth = Console.BufferWidth;
+            int mappedValue = (int)(maxWidth * max);
+            string bar = "";
+            for (int i = 0; i < mappedValue; i++)
+                bar += "#";
+
+            Console.WriteLine(bar);
         }
 
         private static int SelectInputDeviceId()
